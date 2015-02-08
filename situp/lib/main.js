da756e7477ef0e6a4f83376a6c055479ddac6ee9
Rohit Menon <rohitsm@gmail.com>
2015-02-09 02:57:43 +0800

    premilinary working version with intervals of 5 seconds.

--- situp/lib/main.js
+++ situp/lib/main.js
@@ -1,14 +1,24 @@
-var data = require("sdk/self").data;
-// Construct a panel loading its content from the "text-entry.html"
-// file in the "data" directory, and loading the "get-text.js" script
-// into it.
-var text_entry = require("sdk/panel").Panel({
-	contentURL: data.url("text-entry.html"),
-	contentScriptFile: data.url("get-text.js")
-});
+// Initialize and define elements
+var self = require("sdk/self");
+var tmr = require("sdk/timers");
+var panels = require("sdk/panel");
+var buttons = require("sdk/ui/button/toggle");
+var tabs = require("sdk/tabs");
+
+/**
+var tab = tabs.on("ready", logURL);
+
+function logURL(tab){
+	console.log(tab.url);
+}
+
+tabs.on("ready", function() {
+	console.log("tab loaded");
+
+}); */
 
 // Create a button
-require("sdk/ui/button/action").ActionButton({
+var button = buttons.ToggleButton({
 	id: "show-panel",
 	label: "Show Panel",
 	icon: {
@@ -16,27 +26,42 @@ require("sdk/ui/button/action").ActionButton({
 	"32": "./icon-32.png",
 	"64": "./icon-64.png"
 	},
-	onClick: handleClick
+	onChange: activate_al
 });
 
-// Show the panel when the user clicks the button.
-function handleClick(state) {
-  text_entry.show();
+// Create Panels
+var panel = panels.Panel({
+	width: 640,
+	height: 400,
+	contentURL: self.data.url("text-entry.html"),
+	onHide: deactivate_al
+});
+
+// setInterval(activate_al, 1800000)
+
+function activate_al(state) {
+	
+	panel.show({
+		// Pass the button as arg so that panel
+		// attaches itself to the button.
+		position: button 	
+	});
+	console.log("Show Al!");
+	tmr.setTimeout(deactivate_al, 5000);		
+	
 }
 
-// When the panel is displayed it generated an event called
-// "show": we will listen for that event and when it happens,
-// send our own "show" event to the panel's script, so the
-// script can prepare the panel for display.
-text_entry.on("show", function() {
-	text_entry.port.emit("show");
-});
+function deactivate_al() {
+	panel.hide();
+	tmr.setTimeout(activate_al, 5000)
+	console.log("Hid Al!");
+}
 
-// Listen for messages called "text-entered" coming from
-// the content script. The message payload is the text the user
-// entered.
-// In this implementation we'll just log the text to the console.
-text_entry.port.on("text-entered", function (text) {
-	  console.log(text);
-	  text_entry.hide();
-});
+
+/**
+Scarface quote
+
+So you wanna dance, 
+Frank, or do you wanna sit here and have a heart attack?
+
+*/
